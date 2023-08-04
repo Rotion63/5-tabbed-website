558385e2bf8198fb106bf5596ad48688ee44cf94
Madhav Prasad Guragain <guragainroshan63@gmail.com>
2023-08-04 14:29:22 +0545

    added the /url for all

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Load the Home tab content by default
-    loadHomeContent();
+    // Load the Home tab content and update URL by default
+    loadTab(1);
   });
-function openTab(tabIndex) {
+  
+  function openTab(tabIndex) {
+    loadTab(tabIndex);
+    const tabName = getTabName(tabIndex);
+    const url = `/${tabName}`;
+    history.pushState({tabIndex}, null, url);
+  }
+  
+  function loadTab(tabIndex) {
     switch (tabIndex) {
-    case 1:
+      case 1:
         loadHomeContent();
         break;
-    case 2:
+      case 2:
         loadBlogsContent();
         break;
-    case 3:
+      case 3:
         loadAboutContent();
         break;
-    case 4:
+      case 4:
         loadWeeklyChallangesContent();
         break;
-    case 5:
+      case 5:
         loadWinnersContent();
         break;
       // Add cases for other tabs here (3, 4, and 5)
@@ -24,4 +32,28 @@ function openTab(tabIndex) {
         console.log("Invalid tab index.");
     }
   }
+  
+  function getTabName(tabIndex) {
+    switch (tabIndex) {
+      case 1:
+        return "home";
+      case 2:
+        return "blogs";
+      case 3:
+        return "about";
+      case 4:
+        return "weekly_challenges";
+      case 5:
+        return "winners";
+      // Add cases for other tabs here (3, 4, and 5)
+      default:
+        console.log("Invalid tab index.");
+        return "";
+    }
+  }
+  
+  window.addEventListener("popstate", function(event) {
+    const tabIndex = event.state ? event.state.tabIndex : 1;
+    loadTab(tabIndex);
+  });
   
